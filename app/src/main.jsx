deb583bbe131ac7dfd7d803f11c67b73eddadf4c
seiver <seiverlauth@gmail.com>
2026-05-12 12:42:25 -0700

    Remove grain filter, was covering entire page

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,15 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <svg className="grain-filter" xmlns="http://www.w3.org/2000/svg">
-        <filter id="grainy">
-          <feTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch" />
-          <feColorMatrix type="saturate" values="0" />
-          <feBlend in="SourceGraphic" mode="multiply" />
-        </filter>
-      </svg>
-      <App />
-    </>
+    <App />
   </React.StrictMode>,
 )
